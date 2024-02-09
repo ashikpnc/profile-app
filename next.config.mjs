a545ea42e26348b26b4faafe92a7d43ac591335a
ashik <ashikpnc@gmail.com>
2024-02-09 23:56:30 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+    dest: 'public', // Output directory for PWA files
+    register: true, // Register the service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  });
+  
+  module.exports = withPWA({
+    reactStrictMode: true, // Enable React strict mode
+  });
+  
