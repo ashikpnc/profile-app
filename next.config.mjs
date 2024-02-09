560a3ff41f624d8f010bd5e44ed0ffaeaf7e1264
ashik <ashikpnc@gmail.com>
2024-02-10 00:33:54 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-    dest: 'public', // Output directory for PWA files
-    register: true, // Register the service worker
-    skipWaiting: true, // Skip waiting for service worker activation
-    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
-  });
-  
-  module.exports = withPWA({
-    reactStrictMode: true, // Enable React strict mode
-  });
-  
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public", // Output directory for PWA files
+  register: true, // Register the service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
+
+// Your Next config is automatically typed!
+export default withPWA({
+  reactStrictMode: true,
+});
