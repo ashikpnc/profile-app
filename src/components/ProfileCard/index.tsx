8c073698aa2b3c465c5c45dbccbfa112048eaa3a
Muhammed Ashik <ashik.muhammed@athenagt.com>
2024-02-12 17:28:40 +0530

    social links

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Link from "next/link";
 import "./styles.scss";
 import Image from "next/image";
-import profilePic from "@/assets/profile-pic.jpg"
+import { FaInstagram, FaLinkedin, FaTelegram } from "react-icons/fa";
+import profilePic from "@/assets/profile-pic.jpg";
 
 type Props = {};
 
@@ -9,12 +11,22 @@ const ProfileCard = (props: Props) => {
   return (
     <div className="profile-card">
       <div className="profile-picture-container">
-        {/* Replace the image source with your actual profile picture */}
-        <Image  src={profilePic} alt="Profile"  />
+        <Image src={profilePic} alt="Profile" />
       </div>
       <div className="profile-info">
         <h2 className="name">Muhammed Ashik</h2>
         <p className="profession">Software Engineer</p>
+        <div className="social-links">
+          <Link href="https://www.instagram.com/ashikswanderlens">
+            <FaInstagram className="icon" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/ashikqmuhammed">
+            <FaLinkedin className="icon" />
+          </Link>
+          <Link href="https://t.me/ashikqmuhammed">
+            <FaTelegram className="icon" />
+          </Link>
+        </div>
       </div>
     </div>
   );
